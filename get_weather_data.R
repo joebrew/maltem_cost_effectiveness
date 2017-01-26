9a648bd592bc272e92fd874ae66bbd82b91f8185
Joe Brew <joebrew@gmail.com>
2017-01-26 09:16:28 +0200

    better conditoional

--- get_weather_data.R
+++ get_weather_data.R
@@ -20,19 +20,30 @@ dates <- seq(as.Date('2010-01-01'),
              as.Date('2016-12-31'),
              by = 1)
 
+# If no weather_data dir, create on
+if(!dir.exists('weather_data')){
+  create.dir('weather_data')
+}
+
 for (i in 1:length(dates)){
-  this_date <- dates[i]
-  message(this_date)
-  this_url <- create_url(this_date)
-  # Download file
-  download.file(url = this_url,
-                destfile = 'temp.tif.gz')
-  # Extract
-  R.utils::gunzip('temp.tif.gz')
-  # Read as raster
-  r <- raster('temp.tif')
-  # Save
-  save(r,
-       file = paste0('weather_data/', this_date, '.RData'))
-  
+  try({
+    # Define a file name
+    file_name <- paste0('weather_data/', this_date, '.RData')
+    # Skip if the data is already there
+    if(!file.exists(file_name)){
+      this_date <- dates[i]
+      message(this_date)
+      this_url <- create_url(this_date)
+      # Download file
+      download.file(url = this_url,
+                    destfile = 'temp.tif.gz')
+      # Extract
+      R.utils::gunzip('temp.tif.gz')
+      # Read as raster
+      r <- raster('temp.tif')
+      # Save
+      save(r,
+           file = file_name)
+    }
+  })
 }
