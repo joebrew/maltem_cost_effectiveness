617f76fbcacadbbb833a1be2d3a4dcb3399f5b7d
Joe Brew <joebrew@gmail.com>
2017-01-27 18:26:46 +0200

    done with weather data integration

--- get_weather_data.R
+++ get_weather_data.R
@@ -29,7 +29,6 @@ if(!dir.exists('weather_data')){
 for (i in 1:length(dates)){
   this_date <- dates[i]
   start_time <- Sys.time()
-  message(this_date)
   try({
     # Define a file name
     file_name <- paste0('weather_data/', this_date, '.tif')
@@ -59,16 +58,37 @@ for (i in 1:length(dates)){
 # Aggregate weather data
 ###########################
 
-# Get a map of maputo province
-library(cism)
-maputo <- moz2
-maputo <- maputo[which(maputo@data$NAME_1 == 'Maputo'),]
+## Get a map of maputo province
+# library(cism)
+# maputo <- moz2
+# maputo <- maputo[which(maputo@data$NAME_1 == 'Maputo'),]
+# centroids <-
+#   data.frame(district = toupper(maputo@data$NAME_2),
+#              x = coordinates(maputo)[,1],
+#              y = coordinates(maputo)[,2])
 
 # Get the centroid of each district in maputo province
-centroids <-
-  data.frame(district = toupper(maputo@data$NAME_2),
-             x = coordinates(maputo)[,1],
-             y = coordinates(maputo)[,2])
+if('coordinates_of_maputo_districts.csv' %in% dir('public_data')){
+  centroids <- read_csv('public_data/coordinates_of_maputo_districts.csv')
+} else {
+  library(ggmap)
+  centroids <- data_frame(district = c('BOANE',
+                                       'MAGUDE',
+                                       'MANHICA',
+                                       'MARRACUENE',
+                                       'MATOLA',
+                                       'MATUTUINE',
+                                       'MOAMBA',
+                                       'NAMAACHA'))
+  centroids$y <- centroids$x <- NA
+  for (i in 1:nrow(centroids)){
+    coords <- geocode(location = paste0(centroids$district[i], 
+                                        ' DISTRICT, MOZAMBIQUE'))
+    centroids$x[i] <- coords[,1]
+    centroids$y[i] <- coords[,2]
+    write_csv(centroids, 'public_data/coordinates_of_maputo_districts.csv')
+  }
+}
 
 
 # Get a place to store aggregated data
@@ -82,51 +102,66 @@ files <- files[grepl('.tif', files, fixed = TRUE)]
 
 # Go into weather data
 setwd('weather_data/')
-
-# Read in each file and combine
-results <- list()
-for (i in 1:length(files)){
-  this_file <- files[i]
-  this_date <- as.Date(gsub('.tif', '', this_file, fixed = TRUE))
-  r <- raster(this_file)
+if(file.exists('aggregated/precipitation_daily.csv') &
+   file.exists('aggregated/precipitation_weekly.csv') &
+   file.exists('aggregated/precipitation_monthly.csv')){
   
-  # Extract the values
-  x <- raster::extract(r, centroids[,2:3])
+  precipitation_daily <- read_csv('aggregated/precipitation_daily.csv')
+  precipitation_weekly <- read_csv('aggregated/precipitation_weekly.csv')
+  precipitation_monthly <- read_csv('aggregated/precipitation_monthly.csv')
+} else {
   
-  # Create a dataframe output
-  output <- centroids %>%
-    dplyr::select(district)
-  output$date <- this_date
-  output$precipitation <- x
-  results[[i]] <- output
-  message(this_date)
+  # Read in each file and combine
+  results <- list()
+  for (i in 1:length(files)){
+    this_file <- files[i]
+    this_date <- as.Date(gsub('.tif', '', this_file, fixed = TRUE))
+    r <- raster(this_file)
+    
+    # Extract the values
+    x <- raster::extract(r, centroids[,2:3])
+    
+    # Create a dataframe output
+    output <- centroids %>%
+      dplyr::select(district)
+    output$date <- this_date
+    output$precipitation <- x
+    results[[i]] <- output
+    message(this_date)
+  }
+  
+  # Add together all the results
+  precipitation <- bind_rows(results)
+  precipitation <- 
+    precipitation %>%
+    dplyr::select(date, district, precipitation)
+  precipitation$year <- as.numeric(format(precipitation$date, '%Y'))
+  precipitation$month <- as.numeric(format(precipitation$date, '%m'))
+  precipitation$day <- as.numeric(format(precipitation$date, '%d'))
+  precipitation$week <- as.numeric(format(precipitation$date, '%U')) + 1
+  
+  # Clean up names
+  precipitation$district[precipitation$district == 'MANHIÇA'] <-
+    'MANHICA'
+  precipitation$district[precipitation$district == 'MATUTUÍNE'] <-
+    'MATUTUINE'
+  
+  # Aggregate
+  precipitation_daily <- precipitation
+  precipitation_weekly <- 
+    precipitation %>%
+    group_by(year, week, district) %>%
+    summarise(precipitation = sum(precipitation, na.rm = TRUE))
+  precipitation_monthly <- 
+    precipitation %>%
+    group_by(year, month, district) %>%
+    summarise(precipitation = sum(precipitation, na.rm = TRUE))
+  
+  # Store the results
+  write_csv(precipitation_daily, 'aggregated/precipitation_daily.csv')
+  write_csv(precipitation_weekly, 'aggregated/precipitation_weekly.csv')
+  write_csv(precipitation_monthly, 'aggregated/precipitation_monthly.csv')
+  
 }
-
-# Add together all the results
-precipitation <- bind_rows(results)
-precipitation <- 
-  precipitation %>%
-  dplyr::select(date, district, precipitation)
-precipitation$year <- as.numeric(format(precipitation$date, '%Y'))
-precipitation$month <- as.numeric(format(precipitation$date, '%m'))
-precipitation$day <- as.numeric(format(precipitation$date, '%d'))
-precipitation$week <- as.numeric(format(precipitation$date, '%U')) + 1
-
-
-# Aggregate
-precipitation_daily <- precipitation
-precipitation_weekly <- 
-  precipitation %>%
-  group_by(year, week, district) %>%
-  summarise(precipitation = sum(precipitation, na.rm = TRUE))
-precipitation_monthly <- 
-  precipitation %>%
-  group_by(year, month, district) %>%
-  summarise(precipitation = sum(precipitation, na.rm = TRUE))
-
-# Store the results
-write_csv(precipitation_daily, 'aggregated/precipitation_daily.csv')
-write_csv(precipitation_weekly, 'aggregated/precipitation_weekly.csv')
-write_csv(precipitation_monthly, 'aggregated/precipitation_monthly.csv')
 
 setwd('..')
--- master.R
+++ master.R
@@ -11,3 +11,33 @@ df <-
             by = c('district', 'year', 'week'))
 
 write_csv(df, 'data/cleaned/cases_with_precipitation.csv')
+
+library(cism)
+ggplot(data = df,
+       aes(x = date,
+           y = precipitation)) +
+  geom_line(alpha = 0.8,
+            color = 'darkgreen') +
+  facet_wrap(~district) +
+  labs(x = 'Date',
+       y = 'Milimeters',
+       title = 'Rainfall',
+       subtitle = 'Province of Maputo') +
+  theme_cism()
+
+
+cols <- colorRampPalette(brewer.pal(n = 9, 'Spectral'))(length(unique(df$year)))
+ggplot(data = df %>%
+         mutate(day_of_year = as.numeric(format(date, '%j'))),
+       aes(x = day_of_year,
+           y = precipitation)) +
+  geom_line(alpha = 0.8,
+            aes(color = factor(year))) +
+  facet_wrap(~district) +
+  labs(x = 'Day of year',
+       y = 'Milimeters',
+       title = 'Rainfall',
+       subtitle = 'Province of Maputo') +
+  theme_bw() +
+  scale_color_manual(name = 'Year',
+                     values = cols)
